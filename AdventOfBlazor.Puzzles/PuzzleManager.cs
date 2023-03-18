5a072ec00a6d5a29a0b0c1f59ec6775a92930ff9
PezzA <john.d.perrin@gmail.com>
2023-03-18 15:30:56 +0000

    Puzzle grid view (#2)
    * Minor namespace fix and update already on local machine.
    
    * Added attribute class for puzzle data.  Some methods for getting all basic puzzles and the attribute data, and surfaced on FE.

---
+++ AdventOfBlazor.Puzzles/PuzzleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace AdventOfBlazor.Puzzles
+{
+    public class PuzzleManager
+    {
+        public static IEnumerable<PuzzleDataAttribute> GetAllBasicPuzzles()
+        {
+            var puzzleDataList = new List<PuzzleDataAttribute>();
+
+            foreach (Type mytype in Assembly.GetExecutingAssembly().GetTypes()
+                    .Where(mytype => mytype.GetInterfaces().Contains(typeof(IBasicPuzzle))))
+            {
+                if (Attribute.GetCustomAttribute(mytype, typeof(PuzzleDataAttribute)) is PuzzleDataAttribute dataAttribute)
+                {
+                    puzzleDataList.Add(dataAttribute);
+                }
+            }
+
+            return puzzleDataList;
+        }
+    }
+}
---
+++ AdventOfBlazor.Puzzles/PuzzleStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfBlazor.Puzzles
+{
+    public enum PuzzleStatus { 
+        NotYetImplemented = 0,
+        HalfComplete = 1,
+        Complete = 2,
+        HalfCompleteElseWhere = 3,
+        CompleteElseWhere = 4
+    }
+
+}
