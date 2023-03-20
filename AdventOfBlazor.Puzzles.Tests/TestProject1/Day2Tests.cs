a6ec905f888449440abe14277fe9db421d27554a
John Perrin <john.d.perrin@gmail.com>
2023-03-20 22:48:55 +0000

    2022 day 2

---
+++ AdventOfBlazor.Puzzles.Tests/TestProject1/Day2Tests.cs
@@ -0,0 +1,38 @@
+using AdventOfBlazor.Puzzles.Twenty22;
+
+namespace AdventOfBlazor.Puzzles.Tests
+{
+    public class Day2Tests
+    {
+        readonly Day2 puzzle;
+
+        public Day2Tests()
+        {
+            puzzle = new Day2();
+        }
+
+        [Fact]
+        public void TestLoadData()
+        {
+            var data = puzzle.LoadData(puzzle.PuzzleInput());
+
+            Assert.NotNull(data);
+            Assert.Equivalent(new Day2.Instruction { OpponentPlay = "A", MyPlay = "Y" }, data[0]);
+            Assert.Equivalent(new Day2.Instruction { OpponentPlay = "A", MyPlay = "Y" }, data[^1]);
+        }
+
+        [Fact]
+        public void TestPartOne()
+        {
+            var result = puzzle.PartOne(puzzle.PuzzleInput());
+            Assert.Equal("15632", result[0]);
+        }
+
+        [Fact]
+        public void TestPartTwo()
+        {
+            var result = puzzle.PartTwo(puzzle.PuzzleInput());
+            Assert.Equal("14416", result[0]);
+        }
+    }
+}
--- AdventOfBlazor.Puzzles/VanillaDay/Day.cs
+++ AdventOfBlazor.Puzzles/VanillaDay/Day.cs
@@ -2,7 +2,7 @@
 namespace AdventOfBlazor.Puzzles.Twenty22
 {
     [PuzzleData(Year = 0, Day = 0, Title = "", Stars = 0, ImplementedElsewhere = false)]
-    public partial class Day2 : IBasicPuzzle
+    public partial class Day : IBasicPuzzle
     {
 
         public string[] PartOne(string input)
