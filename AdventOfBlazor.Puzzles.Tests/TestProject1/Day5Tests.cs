5548e330a0af5c4ff5a1299e4808992c8d3f9cab
John Perrin <john.d.perrin@gmail.com>
2023-03-26 16:02:59 +0100

    2022 05 Complete

--- AdventOfBlazor.Puzzles.Tests/TestProject1/Day5Tests.cs
+++ AdventOfBlazor.Puzzles.Tests/TestProject1/Day5Tests.cs
@@ -12,17 +12,50 @@ namespace AdventOfBlazor.Puzzles.Tests
         }
 
         [Fact]
+        public void Test_LoadData()
+        {
+            var (stacks, instructions) = puzzle.LoadData(puzzle.PuzzleInput());
+           
+            Assert.NotNull(stacks);
+            Assert.Equal(9, stacks.Keys.Count);
+
+            Assert.Equal('Q', stacks[1].Peek());
+            Assert.Equal('G', stacks[2].Peek());
+            Assert.Equal('B', stacks[3].Peek());
+            Assert.Equal('N', stacks[4].Peek());
+            Assert.Equal('F', stacks[5].Peek());
+            Assert.Equal('J', stacks[6].Peek());
+            Assert.Equal('V', stacks[7].Peek());
+            Assert.Equal('N', stacks[8].Peek());
+            Assert.Equal('M', stacks[9].Peek());
+
+            Assert.NotNull(instructions);
+        }
+
+        [Fact]
+        public void Test_Instruction()
+        {
+            var (stacks, instructions) = puzzle.LoadData(puzzle.PuzzleInput());
+
+            stacks = puzzle.ProcessInstruction(stacks, instructions[0]);
+
+            Assert.Equal(7, stacks[2].Count);
+            Assert.Equal('F', stacks[2].Peek());
+            Assert.Empty(stacks[6]);
+        }
+
+        [Fact]
         public void TestPartOne()
         {
             var result = puzzle.PartOne(puzzle.PuzzleInput());
-            Assert.Equal("", result[0]);
+            Assert.Equal("FJSRQCFTN", result[0]);
         }
 
         [Fact]
         public void TestPartTwo()
         {
             var result = puzzle.PartTwo(puzzle.PuzzleInput());
-            Assert.Equal("", result[0]);
+            Assert.Equal("CJVLJQPHS", result[0]);
         }
     }
 }
