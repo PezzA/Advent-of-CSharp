b95cfa33819f13c8929e07e2ed1bc8bf5b6b68cc
John Perrin <john.d.perrin@gmail.com>
2023-04-30 20:23:20 +0100

    So much messing about!  Dark AoC inspired theme.  toggling of vis based on metadata etc...

---
+++ AdventOfBlazor.Puzzles.Tests/TestProject1/Year15/Tests.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfBlazor.Puzzles.Year15.Day01;
+
+namespace AdventOfBlazor.Puzzles.Tests.Year15.Day01;
+
+public class Tests
+{
+    private readonly Puzzle puzzle;
+
+    public Tests()
+    {
+        puzzle = new Puzzle();
+    }
+
+    [Fact]
+    public void TestPartOne() => Assert.Equal("138", puzzle.PartOne(puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("1771", puzzle.PartTwo(puzzle.PuzzleInput())[0]);
+}
