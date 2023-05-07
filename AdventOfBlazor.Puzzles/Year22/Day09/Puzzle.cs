54cb3e5474cd4c0620e1af895a0717b2fa85fd56
PezzA <john.d.perrin@gmail.com>
2023-05-07 22:26:45 +0100

    changes

--- AdventOfBlazor.Puzzles/Year22/Day09/Puzzle.cs
+++ AdventOfBlazor.Puzzles/Year22/Day09/Puzzle.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfBlazor.Puzzles.Year22.Day09;
+﻿using AdventOfBlazor.Puzzles.Geometery;
+
+namespace AdventOfBlazor.Puzzles.Year22.Day09;
 
 [PuzzleData(Year = 2022, Day = 9, Title = "Rope Bridge", Stars = 0)]
 public partial class Puzzle : IBasicPuzzle
@@ -7,6 +9,37 @@ public partial class Puzzle : IBasicPuzzle
     {
         public string Direction { get; set; } = string.Empty;
         public int Distance { get; set; }
+    }
+
+    public static Point2D MoveTail(Point2D Head, Point2D Tail)
+    {
+        // see if either head has moved too far from the tail
+        var xDiff = Head.X - Tail.X;
+        var yDiff = Head.Y - Tail.Y;
+
+        var xEscape = Math.Abs(xDiff) < 2;
+        var yEscape = Math.Abs(yDiff) < 2;
+
+        if (!xEscape && !yEscape)
+        {
+            return Tail;
+        }
+
+        if (xEscape && !yEscape)
+        {
+            return new Point2D(Tail.X - (xDiff - 1), Tail.Y);
+        }
+
+    }
+
+    public static Dictionary<Point2D, int> ProcessInstructions(List<Instruction> instructions)
+    {
+        var head = new Point2D(0, 0);
+        var tail = new Point2D(0, 0);
+
+
+
+        return new Dictionary<Point2D, int>();
     }
 
     public List<Instruction> LoadData(string input)
