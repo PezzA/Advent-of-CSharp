ce7e94eed8fcde0bc22d5aa0d510f3cc62c5ffb3
John Perrin <john.d.perrin@gmail.com>
2023-09-01 21:57:39 +0100

    2022 13 Complete

---
+++ AdventOfCSharp.Puzzles/Parsing/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCSharp.Puzzles.Parsing
+{
+    public static class StringExtensions
+    {
+        public static string[]? ParseStringArray(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return null;
+
+            return input
+                .Split(Environment.NewLine)
+                .Select(l => l.Replace("\r", "").Trim())
+                .ToArray();
+        }
+    }
+}
