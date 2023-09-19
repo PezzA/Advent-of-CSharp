414c4e3987fa5bd3edfeec8c73448ac548bbc105
PezzA <john.d.perrin@gmail.com>
2023-09-19 19:03:41 +0100

    Part one solved and vix implemented (#37)
    Co-authored-by: John Perrin <jperrin@travelrepublic.co.uk>

---
+++ AdventOfCSharp.Puzzles.Tests/Year22/Day14/Tests.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCSharp.Puzzles.Geometery;
+using AdventOfCSharp.Puzzles.Year22.Day14;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year22.Day14;
+
+public class Tests
+{
+    private readonly Puzzle puzzle;
+
+    public Tests()
+    {
+        puzzle = new Puzzle();
+    }
+
+    public const string TestData = """
+        498,4 -> 498,6 -> 496,6
+        503,4 -> 502,4 -> 502,9 -> 494,9
+        """;
+
+    [Fact]
+    public void LoadsDate()
+    {
+        var paths = puzzle.LoadData(TestData);
+
+        Assert.Equal(2, paths.Count());
+        Assert.Equal(3, paths[0].Count());
+
+        Assert.Equal(new Point2D(498, 4), paths[0][0]);
+        Assert.Equal(new Point2D(498, 6), paths[0][1]);
+        Assert.Equal(new Point2D(496, 6), paths[0][2]);
+    }
+
+    [Fact]
+    public void TestPartOne() => Assert.Equal(Constants.NOT_YET_IMPLEMENTED, puzzle.PartOne(puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal(Constants.NOT_YET_IMPLEMENTED, puzzle.PartTwo(puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year22/Day14/Puzzle.cs
@@ -0,0 +1,160 @@
+﻿using AdventOfCSharp.Puzzles.Geometery;
+using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year22.Day14;
+
+[PuzzleData(Year = 2022, Day = 14, Title = "Regolith Reservoir", HasHTML5Visualisation = true)]
+public partial class Puzzle : IBasicPuzzle
+{
+
+    public enum CellContents
+    {
+        Air,
+        Rock,
+        Sand
+    }
+
+    public List<List<Point2D>> LoadData(string input)
+    {
+        var lines = input.ParseStringArray() ?? throw new ArgumentNullException(nameof(input));
+
+        var paths = new List<List<Point2D>>();
+
+        foreach (var line in lines)
+        {
+            paths.Add(line.Split(" -> ").Select(ParseSegment).ToList());
+        }
+
+        return paths;
+    }
+
+    public Point2D GetTopLeft(Dictionary<Point2D, CellContents> cells)
+    {
+
+        var x = int.MaxValue;
+        var y = int.MaxValue;
+
+        foreach (var cell in cells)
+        {
+            if (cell.Key.X < x) x = cell.Key.X;
+            if (cell.Key.Y < y) y = cell.Key.Y;
+        }
+
+        return new Point2D(x, y);
+
+    }
+
+    public Point2D GetBottomRight(Dictionary<Point2D, CellContents> cells)
+    {
+        var x = 0;
+        var y = 0;
+
+        foreach (var cell in cells)
+        {
+            if (cell.Key.X > x) x = cell.Key.X;
+            if (cell.Key.Y > y) y = cell.Key.Y;
+        }
+
+        return new Point2D(x, y);
+    }
+
+    public Dictionary<Point2D, CellContents> CreateLayout(List<List<Point2D>> paths)
+    {
+        var caves = new Dictionary<Point2D, CellContents>();
+
+        foreach (var path in paths)
+        {
+            for (var i = 1; i < path.Count; i++)
+            {
+                var start = path[i - 1];
+                var end = path[i];
+
+                var x = start.X;
+                var y = start.Y;
+                caves[new Point2D(x, y)] = CellContents.Rock;
+
+                while (true)
+                {
+                    x += TravelIncrement(x, end.X);
+                    y += TravelIncrement(y, end.Y);
+
+                    caves[new Point2D(x, y)] = CellContents.Rock;
+
+                    if (x == end.X && y == end.Y)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return caves;
+    }
+
+    public Point2D DropSand(Dictionary<Point2D, CellContents> cells, Point2D start)
+    {
+        var position = start;
+
+        var br = GetBottomRight(cells);
+
+        while (true)
+        {
+            if (position.Y > br.Y)
+            {
+                return new Point2D(0, 0);
+            }
+
+            var testDown = position.Add(new Point2D(0, 1));
+            var testLeft = position.Add(new Point2D(-1, 1));
+            var testRight = position.Add(new Point2D(1, 1));
+
+            if (!cells.ContainsKey(testDown))
+            {
+                position = testDown;
+                continue;
+            }
+
+            if (!cells.ContainsKey(testLeft))
+            {
+                position = testLeft;
+                continue;
+            }
+
+            if (!cells.ContainsKey(testRight))
+            {
+                position = testRight;
+                continue;
+            }
+
+            break;
+        }
+
+        return position;
+    }
+
+    private int TravelIncrement(int first, int second)
+    {
+        return first == second
+            ? 0
+            : first < second
+                ? 1
+                : -1;
+    }
+
+    private Point2D ParseSegment(string rawSegment)
+    {
+        var bits = rawSegment.Split(',');
+
+        return new Point2D(int.Parse(bits[0]), int.Parse(bits[1]));
+    }
+
+    public string[] PartOne(string input)
+    {
+        return new string[] { Constants.NOT_YET_IMPLEMENTED };
+    }
+
+    public string[] PartTwo(string input)
+    {
+        return new string[] { Constants.NOT_YET_IMPLEMENTED };
+    }
+}
