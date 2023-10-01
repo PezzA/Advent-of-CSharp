fc35893b40112b72c2cf49368505328475d5bdcd
John Perrin <jperrin@travelrepublic.co.uk>
2023-10-01 19:56:00 +0100

    2018 01

---
+++ AdventOfCSharp.Puzzles.Tests/Year18/Day01/Tests.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCSharp.Puzzles.Year18.Day01;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year18.Day01;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle = new();
+
+    [Fact]
+    public void TestPartOne() => Assert.Equal("493", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("413", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year18/Day01/Puzzle.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year18.Day01;
+
+[PuzzleData(Year = 2018, Day = 1, Title = "Chronal Calibration", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+
+    public List<int> LoadData(string input)
+    {
+        var lines = input.ParseStringArray() ?? throw new InvalidDataException("bad input!");
+
+        return lines
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    public string[] PartOne(string input)
+    {
+        return new string[] { LoadData(input).Sum().ToString() };
+    }
+
+    public int FrequencyChange(int start, List<int> changes)
+    {
+        start += changes.Sum();
+
+        return start;
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var hits = new Dictionary<int, bool>();
+        var vals = LoadData(input);
+        var frequency = 0;
+        hits[0] = true;
+
+        while (true)
+        {
+            foreach (var val in vals)
+            {
+                frequency += val;
+                if (hits.ContainsKey(frequency))
+                {
+                    return new string[] { frequency.ToString() };        
+                }
+                hits[frequency] = true;
+            }
+        }
+    }
+}
