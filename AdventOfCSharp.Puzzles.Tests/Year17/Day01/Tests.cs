74e381cd061dd781545810371f07391cb5918a56
John Perrin <jperrin@travelrepublic.co.uk>
2023-10-01 19:10:41 +0100

    2017 01 complete

---
+++ AdventOfCSharp.Puzzles.Tests/Year17/Day01/Tests.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCSharp.Puzzles.Year17.Day01;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year17.Day01;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle;
+
+    public Tests()
+    {
+        _puzzle = new();
+    }
+    
+    [Theory]
+    [InlineData("1122", 3)]
+    [InlineData("1111", 4)]
+    [InlineData("1234", 0)]
+    [InlineData("91212129", 9)]
+    public void Test_PartOne_TestData(string testCase, int expected)
+    {
+        Assert.Equal(_puzzle.PartOne(testCase)[0],expected.ToString());
+    }
+
+    [Theory]
+    [InlineData("1212", 6)]
+    [InlineData("1221", 0)]
+    [InlineData("123425", 4)]
+    [InlineData("123123", 12)]
+    [InlineData("12131415", 4)]
+    public void Test_PartTwo_TestData(string testCase, int expected)
+    {
+        Assert.Equal(_puzzle.PartTwo(testCase)[0],expected.ToString());
+    }
+    
+    [Fact]
+    public void TestPartOne() => Assert.Equal("1044", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("1054", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year17/Day01/Puzzle.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCSharp.Puzzles.Year17.Day01;
+
+[PuzzleData(Year = 2017, Day = 1, Title = "Inverse Captcha", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    public string[] PartOne(string input)
+    {
+        var count = 0;
+        for (var i = 1; i < input.Length; i++)
+        {
+            if (input[i] == input[i - 1])
+            {
+                count+= int.Parse(input[i].ToString());
+            }
+        }
+
+        if (input[0] == input[^1])
+        {
+            count += int.Parse(input[0].ToString());
+        }
+
+        return new string[] { count.ToString()};
+    }
+
+    private int GetWrappedIndex(int length, int divisor, int currPos)
+    {
+        return (currPos + length / divisor) % length;
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var count = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (input[i] == input[GetWrappedIndex(input.Length, 2, i)])
+            {
+                count+= int.Parse(input[i].ToString());
+            }
+        }
+        return new string[] { count.ToString() };
+    }
+}
