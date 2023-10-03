2a0c6f41f603671851abce8cf3be62f9ca1ec45e
John Perrin <jperrin@travelrepublic.co.uk>
2023-10-03 19:56:16 +0100

    2021 01 , all the 1's!

---
+++ AdventOfCSharp.Puzzles.Tests/Year21/Day01/Tests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCSharp.Puzzles.Year21.Day01;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year21.Day01;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle = new();
+
+    private static readonly string TestData = string.Empty;
+    
+    [Fact]
+    public void TestPartOne() => Assert.Equal("1532", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("1571", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year21/Day01/Puzzle.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year21.Day01;
+
+[PuzzleData(Year = 2021, Day = 1, Title = "Sonar Sweep", Stars = 0, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    
+    public static IEnumerable<int> LoadData(string input)
+    {
+        return (input.ParseStringArray() ?? Array.Empty<string>())
+            .Select(int.Parse);
+    }
+
+    public string[] PartOne(string input)
+    {
+        var items = LoadData(input).ToList();
+
+        var count = 0;
+        for (var i = 1; i < items.Count; i++)
+        {
+            if (items[i] > items[i - 1])
+            {
+                count++;
+            }
+        }
+
+        return new [] { count.ToString() };
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var items = LoadData(input).ToList();
+
+        var count = 0;
+        for (var i = 3; i < items.Count; i++)
+        {
+            var cmp1 = items[i-1] + items[i - 2] + items[i - 3];
+            var cmp2 = items[i] + items[i - 1] + items[i - 2];
+            
+            if (cmp2> cmp1)
+            {
+                count++;
+            }
+        }
+
+        return new [] { count.ToString() };
+    }
+}
