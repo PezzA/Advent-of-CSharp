ca4f9cddfda1dbdc3d375b76f0df03159a70c92d
John Perrin <john.d.perrin@gmail.com>
2023-12-02 06:32:45 +0000

    2023 day 02

---
+++ AdventOfCSharp.Puzzles.Tests/Year23/Day02/TemplateTests.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCSharp.Puzzles.Year23.Day02;
+using static AdventOfCSharp.Puzzles.Year23.Day02.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year23.Day02;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle = new();
+
+    private const string TestData
+        = """
+          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+          """;
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var games = LoadData(_puzzle.PuzzleInput());
+        
+        Assert.Equivalent(new Game(1, new Draw[]
+        {
+            new (4,4,16),
+            new (0,5,14),
+            new (1,3,5)
+        } ), games[0]);
+        
+        Assert.Equivalent(new Game(100, new Draw[]
+        {
+            new (3,3,6),
+            new (16,7,2),
+            new (9,9,14),
+            new (9,8,10),
+            new (6,11,0)
+        } ), games[^1]);
+    }
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("8", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("2286", _puzzle.PartTwo(TestData)[0]);
+        
+    [Fact]
+    public void TestPartOne() => Assert.Equal("2156", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("66909", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year23/Day02/Puzzle.cs
@@ -0,0 +1,86 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year23.Day02;
+
+[PuzzleData(Year = 2023, Day = 02, Title = "Cube Conundrum", Stars = 0, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    public record Draw(int Blue, int Red, int Green)
+    {
+        public int Power() => Blue * Red * Green;
+    }
+
+    public record Game(int Id, Draw[] Draws)
+    {
+        public bool IsPossible(int maxRed, int maxGreen, int maxBlue) =>
+            Draws.All(draw => draw.Red <= maxRed && draw.Green <= maxGreen && draw.Blue <= maxBlue);
+
+        public Draw MinDraw() => 
+            new (Draws.Max(d => d.Blue), Draws.Max(d => d.Red), Draws.Max(d => d.Green));        
+    }
+
+    // As ever, parsing the puzzle input assumes valid input.
+    public static Game[] LoadData(string input)
+    {
+        var lines = input.ParseStringArray() ?? throw new Exception("Could not parse string data");
+
+        var games = new List<Game>();
+
+        foreach (var line in lines)
+        {
+            var bits = line.Split(':');
+            var id = int.Parse(bits[0].Split(' ')[1]);
+
+            var draws = bits[1]
+                .Split(';')
+                .Select(StringToDraw)
+                .ToArray();
+
+            games.Add(new Game(id, draws));
+        }
+
+        return games.ToArray();
+    }
+
+    private static Draw StringToDraw(string input)
+    {
+        var bits = input.Split(',');
+        int blue = 0, red = 0, green = 0;
+
+        foreach (var bit in bits)
+        {
+            var parts = bit.Trim().Split(' ');
+
+            switch (parts[1])
+            {
+                case "blue":
+                    blue = int.Parse(parts[0]);
+                    break;
+                case "green":
+                    green = int.Parse(parts[0]);
+                    break;
+                case "red":
+                    red = int.Parse(parts[0]);
+                    break;
+                default:
+                    throw new Exception("unknown colour in draw");
+            }
+        }
+
+        return new Draw(blue, red, green);
+    }
+
+    public string[] PartOne(string input)
+    {
+        var games = LoadData(input);
+
+        return new[] { games.Sum(game => game.IsPossible(12,13,14) ? game.Id : 0).ToString()};
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var games = LoadData(input);
+
+        return new[] { games.Sum(game => game.MinDraw().Power()).ToString()};
+    }
+}
