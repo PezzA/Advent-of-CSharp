7436a1455a9df68d1aedd0f2ba851e56f8cca3ab
PezzA <john.d.perrin@gmail.com>
2023-12-04 07:08:38 +0000

    2023 Day 4 (#45)

---
+++ AdventOfCSharp.Puzzles.Tests/Year23/Day04/Tests.cs
@@ -0,0 +1,41 @@
+﻿using Puzzle = AdventOfCSharp.Puzzles.Year23.Day04.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year23.Day04;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle = new();
+
+    private const string TestData
+        = """
+          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+          Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+          Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+          """;
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var cards = Puzzle.LoadData(TestData);
+
+        Assert.Equivalent(cards[0], new Puzzle.Card(
+            1,
+            new[] { 41, 48, 83, 86, 17 },
+            new[] { 83, 86, 6, 31, 17, 9, 48, 53 }));
+    }
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("13", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("30", _puzzle.PartTwo(TestData)[0]);
+
+    [Fact]
+    public void TestPartOne() => Assert.Equal("15205", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("6189740", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year23/Day04/Puzzle.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year23.Day04;
+
+[PuzzleData(Year = 2023, Day = 4, Title = "Scratchcards", Stars = 0, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    public record Card(int Id, int[] WinningNumbers, int[] CardNumbers)
+    {
+        public int Score()
+        {
+            var winCount = CardNumbers.Count(number => WinningNumbers.Contains(number));
+
+            return winCount > 0 ? Doubler(winCount) : 0;
+        }
+
+        public int[] Wins() => CardNumbers.Where(number => WinningNumbers.Contains(number)).ToArray();
+    }
+
+    private static int Doubler(int iterations, int startValue = 1)
+    {
+        for (var i = 0; i < iterations - 1; i++)
+        {
+            startValue *= 2;
+        }
+
+        return startValue;
+    }
+
+    // As ever, LoadData dosen't concern itself about invalid data
+    public static Card[] LoadData(string input)
+    {
+        var lines = input.ParseStringArray() ?? throw new Exception("could not parse input");
+
+        var cardList = new List<Card>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(':');
+            var id = int.Parse(parts[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)[1].Trim());
+            var lists = parts[1].Trim().Split('|');
+
+            var winningNumbers = lists[0].Trim()
+                .Split(' ')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .ToArray();
+
+            var cardNumbers = lists[1].Trim()
+                .Split(' ')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .ToArray();
+
+            cardList.Add(new Card(id, winningNumbers, cardNumbers));
+        }
+
+        return cardList.ToArray();
+    }
+
+    public string[] PartOne(string input)
+    {
+        var cards = LoadData(input);
+
+        var total = 0;
+        foreach (var card in cards)
+        {
+            total += card.Score();
+        }
+
+        return new[] { total.ToString() };
+    }
+
+    private void Score(Card card, Dictionary<int, int> counts)
+    {
+        for(var i =1; i <= card.Wins().Length; i++)
+        {
+            counts[card.Id+i] += 1;
+        }
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var cards = LoadData(input);
+
+        Dictionary<int, int> counts = new();
+            
+        foreach (var card in cards)
+        {
+            counts[card.Id] = 0;
+        }
+        
+        foreach (var card in cards)
+        {
+            counts[card.Id] += 1;
+
+            for (var i = 0; i < counts[card.Id]; i++)
+            {
+                Score(card, counts);
+            }
+        }
+
+        var total = counts.Sum(x => x.Value);
+        
+        return new[] { total.ToString() };
+    }
+}
