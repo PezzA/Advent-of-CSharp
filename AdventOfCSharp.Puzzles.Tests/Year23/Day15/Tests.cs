079dca67a8955ec0352ddd74863a3f371c7bb637
PezzA <john.d.perrin@gmail.com>
2023-12-16 18:49:45 +0000

    2023 15 (#55)
    * 2023 Day 15 Complete!
    
    * Stars!

---
+++ AdventOfCSharp.Puzzles.Tests/Year23/Day15/Tests.cs
@@ -0,0 +1,42 @@
+﻿using Puzzle = AdventOfCSharp.Puzzles.Year23.Day15.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year23.Day15;
+
+public class Tests
+{
+    private readonly Puzzle _puzzle = new();
+
+    private const string TestData
+        = """
+          rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+          """;
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var items = _puzzle.LoadData(TestData);
+        
+        Assert.Equal(52, _puzzle.GetHash("HASH"));
+        Assert.Equal(30,_puzzle.GetHash(items[0]));
+    }
+
+    [Fact]
+    public void Gets_HashMap()
+    {
+        var items = _puzzle.LoadData(TestData);
+
+        var hashMap = _puzzle.SetupLens(items);
+    } 
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("1320", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("145", _puzzle.PartTwo(TestData)[0]);
+        
+    [Fact]
+    public void TestPartOne() => Assert.Equal("506869", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("271384", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year23/Day15/Puzzle.cs
@@ -0,0 +1,120 @@
+﻿
+namespace AdventOfCSharp.Puzzles.Year23.Day15;
+
+[PuzzleData(Year = 2023, Day = 15, Title = "Lens Library", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    public record Lens(string Label, int Length, bool Remove);
+
+    public string[] LoadData(string input)
+    {
+        return input.Split(',');
+    }
+
+    public int GetHash(string input)
+    {
+        var hash = 0;
+
+        foreach (var hashChar in input)
+        {
+            hash += hashChar;
+            hash *= 17;
+            hash %= 256;
+        }
+
+        return hash;
+    }
+
+    public static Lens GetAddInstruction(string input)
+    {
+        var bits = input.Split('=');
+
+        return new Lens(bits[0], int.Parse(bits[1]), false);
+    }
+
+    public static Lens GetRemoveInstruction(string input)
+    {
+        return new Lens(input.Substring(0, input.Length - 1), 0, true);
+    }
+
+    public Dictionary<int, LinkedList<Lens>> SetupLens(string[] input)
+    {
+        var hashMap = new Dictionary<int, LinkedList<Lens>>();
+
+        for (var index = 0; index < 256; index++)
+        {
+            hashMap[index] = new LinkedList<Lens>();
+        }
+
+        foreach (var item in input)
+        {
+            var parsedItem = item.Contains('=')
+                ? GetAddInstruction(item)
+                : GetRemoveInstruction(item);
+
+            var box = GetHash(parsedItem.Label);
+
+            if (!parsedItem.Remove)
+            {
+                if (hashMap[box].Count == 0)
+                {
+                    hashMap[box].AddFirst(parsedItem);
+                    continue;
+                }
+
+                var found = false;
+                for (var node = hashMap[box].First; node != null; node = node.Next)
+                {
+                    if (node.Value.Label != parsedItem.Label) continue;
+
+                    node.Value = parsedItem;
+                    found = true;
+                    break;
+                }
+
+                if (!found)
+                {
+                    hashMap[box].AddLast(parsedItem);
+                }
+
+                continue;
+            }
+
+            for (var node = hashMap[box].First; node != null; node = node.Next)
+            {
+                if (node.Value.Label == parsedItem.Label)
+                {
+                    hashMap[box].Remove(node);
+                }
+            }
+        }
+
+        return hashMap;
+    }
+
+    public string[] PartOne(string input)
+    {
+        var data = LoadData(input);
+
+        return new[] { data.Sum(GetHash).ToString() };
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var data = LoadData(input);
+
+        var hashMap = SetupLens(data);
+        var total = 0;
+        foreach (var (key, value) in hashMap)
+        {
+            var index = 1;
+            for (var node = value.First; node != null; node = node.Next)
+            {
+                total += (key + 1) * index * node.Value.Length;
+                index += 1;
+            }
+        }
+
+        return new[] { total.ToString()};
+    }
+}
