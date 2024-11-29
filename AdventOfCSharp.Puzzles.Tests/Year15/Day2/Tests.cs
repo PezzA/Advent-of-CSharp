1aafdd4312b29e6885d5e3b23d7140470b4b2577
PezzA <john.d.perrin@gmail.com>
2024-11-29 22:01:47 +0000

    Year 2015 Day 02

---
+++ AdventOfCSharp.Puzzles.Tests/Year15/Day2/Tests.cs
@@ -0,0 +1,48 @@
+﻿using static AdventOfCSharp.Puzzles.Year15.Day02.Puzzle;
+using Puzzle = AdventOfCSharp.Puzzles.Year15.Day02.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year15.Day02;
+
+public class Tests
+{
+    private readonly IBasicPuzzle _puzzle;
+
+    private const string TestData
+        = """
+          2x3x4
+          """;
+
+    public Tests() 
+    {
+        _puzzle = new Puzzle();
+    }
+
+
+    [Theory]
+    [InlineData(52, 2, 3, 4)]
+    [InlineData(42, 1, 1, 10)]
+    public void Box_Calculates_Surface_Area(int expectedResult, int length, int width, int height)
+    {
+        var box = new Box(length, width, height);
+
+        Assert.Equal(expectedResult, box.SurfaceArea);
+    }
+
+    [Fact]
+    public void Loads_Data()
+    {
+        
+    }
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("58", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("34", _puzzle.PartTwo(TestData)[0]);
+        
+    [Fact]
+    public void TestPartOne() => Assert.Equal("1588178", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("3783758", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year15/Day02/Puzzle.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year15.Day02;
+
+[PuzzleData(Year = 2015, Day = 2, Title = "I Was Told There Would Be No Math", Stars = 0, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+    public record Box(int Length, int Width, int Height)
+    {
+        public int SurfaceArea => (2 * Length * Width) + (2 * Width * Height) + (2 * Height * Length);
+
+        public int SmallestSide
+        {
+            get
+            {
+                List<int> sides = [Length, Width, Height];
+
+                sides.Sort();
+
+                return sides[0] * sides[1];
+            }
+        }
+
+        public int SmallestPerimeter
+        {
+            get
+            {
+                List<int> sides = [Length, Width, Height];
+
+                sides.Sort();
+
+                return (sides[0] * 2) + (sides[1] * 2);
+            }
+        }
+
+        public int Volume => Length * Height * Width;
+    }
+
+    public static List<Box> LoadData(string input)
+    {
+        var retVal = new List<Box>();
+
+        var lines = input.ParseStringArray() ?? throw new Exception("Could not parse data");
+
+        return lines
+            .Select(ParseBox)
+            .ToList();
+    }
+
+    public static Box ParseBox(string input)
+    {
+        var bits = input.Split("x");
+
+        return new Box(int.Parse(bits[0]), int.Parse(bits[1]), int.Parse(bits[2]));
+    }
+
+    public string[] PartOne(string input)
+    {
+        var boxes = LoadData(input);
+
+        return [boxes.Sum(b => b.SurfaceArea + b.SmallestSide).ToString()];
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var boxes = LoadData(input);
+
+        return [boxes.Sum(b => b.SmallestPerimeter + b.Volume).ToString()];
+    }
+}
