ca653685a1d69865e6c0b6ffc469413877f6944e
PezzA <john.d.perrin@gmail.com>
2024-12-01 09:30:04 +0000

    2024 01

---
+++ AdventOfCSharp.Puzzles.Tests/Year24/Day01/Tests.cs
@@ -0,0 +1,44 @@
+﻿using Puzzle = AdventOfCSharp.Puzzles.Year24.Day01.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year24.Day01;
+
+public class Tests
+{
+    private readonly IBasicPuzzle _puzzle;
+
+    private const string TestData
+        = """
+          3   4
+          4   3
+          2   5
+          1   3
+          3   9
+          3   3
+          """;
+
+    public Tests() 
+    {
+        _puzzle = new Puzzle();
+    }
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var (fl, sl) = Puzzle.LoadData(TestData);
+
+        Assert.Equal(3, fl[0]);
+        Assert.Equal(4, sl[0]);
+    }
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("11", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("31", _puzzle.PartTwo(TestData)[0]);
+        
+    [Fact]
+    public void TestPartOne() => Assert.Equal("2756096", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("23117829", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year24/Day01/Puzzle.cs
@@ -0,0 +1,73 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCSharp.Puzzles.Year24.Day01;
+
+[PuzzleData(Year = 2024, Day = 1, Title = "Historian Hysteria", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+
+    [GeneratedRegex(@"(?<first>\d*)   (?<second>\d*)")]
+    private static partial Regex InputRegex();
+
+    public static (List<int>, List<int>) LoadData(string input)
+    {
+        var lines = input.ParseStringArray() ?? throw new Exception("Could not parse data");
+
+        var firstList = new List<int>();
+        var secondList = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var match = InputRegex().Match(line);
+
+            if (!match.Success)
+            {
+                throw new InvalidDataException($"Could not parse line: {line}");
+            }
+
+            firstList.Add(int.Parse(match.Groups["first"].Value));
+            secondList.Add(int.Parse(match.Groups["second"].Value));
+        }
+
+        return (firstList, secondList);
+    }
+
+    public string[] PartOne(string input)
+    {
+        var (fl, sl) = LoadData(input);
+
+        fl.Sort();
+        sl.Sort();
+
+        var sum = 0;
+
+        for (int i = 0; i < fl.Count; i++)
+        {
+            sum += Math.Abs(fl[i] - sl[i]);
+        }
+
+        return [sum.ToString()];
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var (fl, sl) = LoadData(input);
+
+        fl.Sort();
+        sl.Sort();
+
+        var sum = 0;
+
+        for (int i = 0; i < fl.Count; i++)
+        {
+            var secondListHits = sl.Count(slItem => slItem == fl[i]);
+
+            sum += fl[i] * secondListHits;
+        }
+
+        return [sum.ToString()];
+    }
+
+
+}
