821b9a630e9deeb1e1d806c4232b3e6e0f6e11fb
PezzA <john.d.perrin@gmail.com>
2024-12-06 11:56:44 +0000

    2024 Days 02,03

---
+++ AdventOfCSharp.Puzzles.Tests/Year24/Day02/Tests.cs
@@ -0,0 +1,54 @@
+﻿using Puzzle = AdventOfCSharp.Puzzles.Year24.Day02.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year24.Day02;
+
+public class Tests
+{
+    private readonly IBasicPuzzle _puzzle;
+
+    private const string TestData
+        = """
+          7 6 4 2 1
+          1 2 7 8 9
+          9 7 6 2 1
+          1 3 2 4 5
+          8 6 4 4 1
+          1 3 6 7 9
+          """;
+
+    public Tests()
+    {
+        _puzzle = new Puzzle();
+    }
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var lists = Puzzle.LoadData(TestData);
+        Assert.NotNull(lists);
+        Assert.Equivalent(new int[] { 7, 6, 4, 2, 1 }, lists.First());
+        Assert.Equivalent(new int[] { 1, 3, 6, 7, 9 }, lists.Last());
+    }
+
+    [Fact]
+    public void TestPartOne_TestData()
+    {
+        var lists = Puzzle.LoadData(TestData);
+
+        Assert.True(Puzzle.IsSafe(lists[0]));
+        Assert.False(Puzzle.IsSafe(lists[1]));
+        Assert.False(Puzzle.IsSafe(lists[2]));
+        Assert.False(Puzzle.IsSafe(lists[3]));
+        Assert.False(Puzzle.IsSafe(lists[4]));
+        Assert.True(Puzzle.IsSafe(lists[5]));
+    }
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("4", _puzzle.PartTwo(TestData)[0]);
+
+    [Fact]
+    public void TestPartOne() => Assert.Equal("371", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("426", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year24/Day02/Puzzle.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCSharp.Puzzles.Parsing;
+
+namespace AdventOfCSharp.Puzzles.Year24.Day02;
+
+[PuzzleData(Year = 2024, Day = 2, Title = "Red-Nosed Reports", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+
+    public static List<List<int>> LoadData(string input)
+    {
+        var lists = new List<List<int>>();
+
+        var lines = input.ParseStringArray() ?? throw new Exception("Could not parse data");
+
+        foreach (var line in lines)
+        {
+            var list = line
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            lists.Add(list);
+        }
+
+        return lists;
+    }
+
+    public enum ListOrder
+    {
+        Ascending,
+        Descending
+    }
+
+    public static bool IsSafe(List<int> input)
+    {
+        if (input == null || input.Count < 2) return false;
+
+        var order = input[0] > input[1]
+            ? ListOrder.Descending
+            : ListOrder.Ascending;
+
+        for (var i = 0; i < (input.Count - 1); i++)
+        {
+            var diff = Math.Abs(input[i] - input[i + 1]);
+
+            if (diff > 3 || diff == 0) return false;
+
+            if (order == ListOrder.Ascending && input[i] > input[i + 1]) return false;
+            if (order == ListOrder.Descending && input[i] < input[i + 1]) return false;
+        }
+
+        return true;
+    }
+
+    public static List<int> StripIndex(List<int> input, int index) 
+    {
+        var retVal = new List<int>();
+
+        for (var i = 0; i < input.Count; i++) 
+        {
+            if (i != index) { 
+                retVal.Add(input[i]);
+            }
+        }
+
+        return retVal;
+    }
+
+    public static bool IsSafeWithRemoval(List<int> input)
+    {
+        if (IsSafe(input)) return true;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            if (IsSafe(StripIndex(input, i))) return true;
+        }
+
+        return false;
+    }
+
+    public string[] PartOne(string input)
+    {
+        var lists = LoadData(input);
+
+        var sum = 0;
+
+        foreach (var list in lists)
+        {
+            if (IsSafe(list))
+            {
+                sum++;
+            }
+
+        }
+
+        return [sum.ToString()];
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var lists = LoadData(input);
+
+        var sum = 0;
+
+        foreach (var list in lists)
+        {
+            if (IsSafeWithRemoval(list))
+            {
+                sum++;
+            }
+
+        }
+
+        return [sum.ToString()];
+    }
+}
