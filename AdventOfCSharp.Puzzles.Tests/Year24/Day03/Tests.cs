38ac85bc4c432ba16487ff401c8b6105bd8cbbba
PezzA <john.d.perrin@gmail.com>
2024-12-06 12:03:26 +0000

    2024 Days 02,03 (#62)
    * 2024 Days 02,03
    
    * fixed test
    
    * updated star count

---
+++ AdventOfCSharp.Puzzles.Tests/Year24/Day03/Tests.cs
@@ -0,0 +1,49 @@
+﻿
+using static AdventOfCSharp.Puzzles.Year24.Day03.Puzzle;
+using Puzzle = AdventOfCSharp.Puzzles.Year24.Day03.Puzzle;
+
+namespace AdventOfCSharp.Puzzles.Tests.Year24.Day3;
+
+public class Tests
+{
+    private readonly IBasicPuzzle _puzzle;
+
+    private const string TestData
+        = """
+          xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+          """;
+
+
+    private const string TestDataPart2
+        = """
+          xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+          """;
+
+    public Tests() 
+    {
+        _puzzle = new Puzzle();
+    }
+
+    [Fact]
+    public void Loads_Data()
+    {
+        var instructions = LoadData(TestData);
+
+        Assert.Equal(4, instructions.Count);
+
+        Assert.Equal(new Instruction(InstructionType.Mul, 2, 4), instructions[0]);
+        Assert.Equal(new Instruction(InstructionType.Mul, 8, 5), instructions[3]);
+    }
+
+    [Fact]
+    public void TestPartOne_TestData() => Assert.Equal("161", _puzzle.PartOne(TestData)[0]);
+
+    [Fact]
+    public void TestPartTwo_TestData() => Assert.Equal("48", _puzzle.PartTwo(TestDataPart2)[0]);
+        
+    [Fact]
+    public void TestPartOne() => Assert.Equal("187194524", _puzzle.PartOne(_puzzle.PuzzleInput())[0]);
+
+    [Fact]
+    public void TestPartTwo() => Assert.Equal("127092535", _puzzle.PartTwo(_puzzle.PuzzleInput())[0]);
+}
---
+++ AdventOfCSharp.Puzzles/Year24/Day03/Puzzle.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCSharp.Puzzles.Year24.Day03;
+
+[PuzzleData(Year = 2024, Day = 3, Title = "Mull It Over", Stars = 2, ImplementedElsewhere = false)]
+public partial class Puzzle : IBasicPuzzle
+{
+
+    public record Instruction(InstructionType Instuction, int Left, int Right);
+
+    [GeneratedRegex(@"(?<ins>mul)\((?<left>\d*),(?<right>\d*)\)|(?<ins>do)\(\)|(?<ins>don't)\(\)")]
+    private static partial Regex InputRegex();
+
+    public enum InstructionType
+    {
+        Mul,
+        Do,
+        Dont
+    }
+
+    public static List<Instruction> LoadData(string input)
+    {
+        var retval = new List<Instruction>();
+
+        var matches = InputRegex().Matches(input);
+
+        foreach (Match match in matches)
+        {
+
+            switch (match.Groups["ins"].Value)
+            {
+                case "mul":
+                    retval.Add(new Instruction(InstructionType.Mul, int.Parse(match.Groups["left"].Value), int.Parse(match.Groups["right"].Value)));
+                    break;
+                case "do":
+                    retval.Add(new Instruction(InstructionType.Do, 0, 0));
+                    break;
+                case "don't":
+                    retval.Add(new Instruction(InstructionType.Dont, 0, 0));
+                    break;
+                default:
+                    throw new Exception($"invalid instruction type {match.Groups["ins"].Value}");
+
+            }
+
+        }
+
+        return retval;
+    }
+
+
+    public string[] PartOne(string input)
+    {
+        var instructions = LoadData(input);
+
+        return [instructions.Sum(i => i.Left * i.Right).ToString()];
+    }
+
+    public string[] PartTwo(string input)
+    {
+        var instructions = LoadData(input);
+
+        var sum = 0;
+
+        var enabled = true;
+        for (int i = 0; i < instructions.Count; i++)
+        {
+            if (enabled && instructions[i].Instuction == InstructionType.Mul)
+            {
+                sum += instructions[i].Left * instructions[i].Right;
+                continue;
+            }
+
+            if (instructions[i].Instuction == InstructionType.Do)
+            {
+                enabled = true;
+                continue;
+            }
+
+            // only other option.
+            enabled = false;
+        }
+
+        return [sum.ToString()];
+    }
+}
