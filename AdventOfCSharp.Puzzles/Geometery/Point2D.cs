340df98a17cce5b63c7eef396c5dc18748a7e91f
PezzA <john.d.perrin@gmail.com>
2024-12-07 23:27:59 +0000

    2024 day 6 (#65)

--- AdventOfCSharp.Puzzles/Geometery/Point2D.cs
+++ AdventOfCSharp.Puzzles/Geometery/Point2D.cs
@@ -6,8 +6,10 @@ namespace AdventOfCSharp.Puzzles.Geometery
     [DebuggerDisplay("[{X},{Y}]")]
     public record Point2D(int X, int Y)
     {
+        public static Point2D Origin = new(0, 0);
+
         public Point2D Add(Point2D point) => new(X + point.X, Y + point.Y);
-        
+
         public static Point2D operator *(Point2D a, int b) => new Point2D(a.X * b, a.Y * b);
 
         public static Point2D operator +(Point2D a, Point2D b) => new Point2D(a.X + b.X, a.Y + b.Y);
@@ -20,12 +22,31 @@ namespace AdventOfCSharp.Puzzles.Geometery
         /// <summary>
         /// TermNorth gets the 'Terminal Screen' north.  Which is descending y as 0 is the top row
         /// </summary>
+        /// 
         public Point2D TermNorth() => this with { Y = Y - 1 };
 
         public Point2D TermSouth() => this with { Y = Y + 1 };
-        
+
         public Point2D TermEast() => this with { X = X + 1 };
-        
+
         public Point2D TermWest() => this with { X = X - 1 };
+
+
+        public static Point2D BearingNorth => new(0, -1);
+
+        public static Point2D BearingSouth => new(0, 1);
+
+        public static Point2D BearingEast => new(1, 0);
+
+        public static Point2D BearingWest => new(-1, 0);
+
+        public static Point2D TurnRight(Point2D input) => input switch
+        {
+            ( 0, -1) => BearingEast,
+            ( 1,  0) => BearingSouth,
+            ( 0,  1) => BearingWest,
+            (-1,  0) => BearingNorth,
+            _ => throw new ArgumentException("Input was not a valid bearing")
+        };
     }
 }
